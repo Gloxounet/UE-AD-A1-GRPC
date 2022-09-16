bdc4ca5e815e44d375529773bd9e94f18c51eeed
Octave Boëlle <octave.boelle@etu.u-bordeaux.fr>
2022-09-16 11:50:35 +0200

    Showtime gRPC implementation

--- showtime/showtime.py
+++ showtime/showtime.py
@@ -10,6 +10,18 @@ class ShowtimeServicer(showtime_pb2_grpc.ShowtimeServicer):
         with open('{}/data/times.json'.format("."), "r") as jsf:
             self.db = json.load(jsf)["schedule"]
 
+    def GetMoviesByDate(self, request, context):
+        for date in self.db:
+            if date['date'] == request.date:
+                print("Date found!")
+                return showtime_pb2.Schedule(date=date['date'], movie=date['movies'])
+        return showtime_pb2.Schedule(date="", movie="")
+
+    def GetTimes(self, request, context):
+        for date in self.db:
+            yield showtime_pb2.Schedule(date=date['date'], movie=date['movies'])
+
+
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     showtime_pb2_grpc.add_ShowtimeServicer_to_server(ShowtimeServicer(), server)
