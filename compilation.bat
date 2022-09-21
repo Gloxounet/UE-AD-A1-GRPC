cd20afa1484ea77c7b442853dc3e19f4981844be
gloxounet <matteo.glock@laposte.net>
2022-09-21 10:17:32 +0200

    pretty

--- compilation.bat
+++ compilation.bat
@@ -1,7 +1,9 @@
 @echo off
 
 for %%t in (user, booking, showtime, movie) do (
+    echo Handling %%t...
     python -m grpc_tools.protoc -I=./protos --python_out=./%%t/ --grpc_python_out=./%%t/ %%t.proto
     copy /y ".\%%t\%%t_pb2_grpc.py" ".\client"
     copy /y ".\%%t\%%t_pb2.py" ".\client"
 )
+echo done.
