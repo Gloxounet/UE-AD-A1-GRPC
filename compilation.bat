a3e27d61fb81622a5793e950c23b88e99f5ca51e
gloxounet <matteo.glock@laposte.net>
2022-09-21 10:37:57 +0200

    dependencies

--- compilation.bat
+++ compilation.bat
@@ -8,3 +8,13 @@ for %%t in (user, booking, showtime, movie) do (
     copy /y ".\%%t\pb2\%%t_pb2.py" ".\client"
 )
 echo done.
+
+echo Handling dependecies...
+for %%d in (base) do (
+    for %%t in (user, booking, showtime, movie) do (
+        echo Handling dependecy %%d in %%t...
+        python -m grpc_tools.protoc -I=./protos --python_out=./%%t/pb2/ --grpc_python_out=./%%t/pb2/ %%d.proto
+    )
+    REM Copie des dépendances dans le client
+    python -m grpc_tools.protoc -I=./protos --python_out=./client/ --grpc_python_out=./client/ %%d.proto
+)
