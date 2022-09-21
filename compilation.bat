a8b7a66f8670004fd100f45c17cd157cc46223e5
gloxounet <matteo.glock@laposte.net>
2022-09-21 14:05:02 +0200

    compil

--- compilation.bat
+++ compilation.bat
@@ -10,13 +10,8 @@ for %%t in (user, booking, showtime, movie) do (
 echo done.
 
 echo Handling dependecies...
-for %%d in (base) do (
-    for %%t in (user, booking, showtime, movie) do (
-        echo Handling dependecy %%d in %%t...
-        python -m grpc_tools.protoc -I=./protos --python_out=./%%t/pb2/ --grpc_python_out=./%%t/pb2/ %%d.proto
-    )
-    REM Copie des dépendances dans le client
-    python -m grpc_tools.protoc -I=./protos --python_out=./client/ --grpc_python_out=./client/ %%d.proto
+for %%t in (user, booking, showtime, movie) do (
+    echo Handling dependecy %%d in %%t...
+    python get_imports.py ./protos/%%t.proto |
+    python -m grpc_tools.protoc -I=./protos --python_out=./%%t/pb2/ --grpc_python_out=./%%t/pb2/ %%d.proto
 )
-
-echo Handling custom dependecies...
